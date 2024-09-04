0763fc0e9e1332b17002bcbbbd8bd366c46e511a
Anubhav Sharma <anubhavs@Anubhavs-MacBook-Pro.local>
2024-09-04 10:55:53 +1000

    header updates

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -102,7 +102,7 @@ export default async function decorate(block) {
   nav.id = 'nav';
   while (fragment.firstElementChild) nav.append(fragment.firstElementChild);
 
-  const classes = ['brand', 'sections', 'tools'];
+  const classes = ['brand', 'sections', 'links', 'tools'];
   classes.forEach((c, i) => {
     const section = nav.children[i];
     if (section) section.classList.add(`nav-${c}`);
